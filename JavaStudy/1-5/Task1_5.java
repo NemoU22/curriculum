17d6b280ce919fbc18c4a140edc4f8b51d6b9269
NemoU22 <107683749+NemoU22@users.noreply.github.com>
2022-08-06 15:38:06 +0900

    課題提出
    Check.java の作成

--- JavaStudy/1-5/Task1_5.java
+++ JavaStudy/1-5/Task1_5.java
@@ -42,8 +42,9 @@ public class Task1_5 {
         
         for (i = 1; i < 10; i++){
             System.out.print(i + " || ");
-            for (int j = 1; j < 10; j++)
+            for (int j = 1; j < 10; j++){
             System.out.print(i*j + " | ");
+            }
             System.out.println();
         }
     }
